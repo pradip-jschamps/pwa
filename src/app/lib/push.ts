c8c660b98e118c2a8116a12cad8bf868a785a3fb
pradip.jschamp <pradip.jschamp@gmail.com>
2025-11-29 14:20:58 +0530

    update in notification setting

--- src/app/lib/push.ts
+++ src/app/lib/push.ts
@@ -1,11 +1,18 @@
+import { urlBase64ToUint8Array } from "@/utils/pwa";
+
+
 export async function subscribeUser() {
   const permission = await Notification.requestPermission();
-  if (permission !== "granted") {
-    alert("Notification permission denied");
-    return;
-  }
+  if (permission !== "granted") return;
 
   const reg = await navigator.serviceWorker.ready;
+
+  // ⛔ VERY IMPORTANT — check existing subscription
+  const existing = await reg.pushManager.getSubscription();
+  if (existing) {
+    console.log("Already subscribed");
+    return existing;
+  }
 
   const sub = await reg.pushManager.subscribe({
     userVisibleOnly: true,
@@ -21,14 +28,6 @@ export async function subscribeUser() {
   });
 
   alert("Push Notification Enabled!");
-}
 
-function urlBase64ToUint8Array(base64String: string) {
-  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding)
-    .replace(/-/g, "+")
-    .replace(/_/g, "/");
-
-  const rawData = atob(base64);
-  return Uint8Array.from([...rawData].map(c => c.charCodeAt(0)));
+  return sub;
 }
